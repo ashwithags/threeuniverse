f4c19b572b1500f0082b6bf31409c9a17ead5c9b
ashwithags <ashwithags@gmail.com>
2018-06-30 22:59:52 +0530

    Update cube.js

--- src/universe_parts/cube.js
+++ src/universe_parts/cube.js
@@ -1,7 +1,7 @@
 defineThreeUniverse(function (THREE) {
 
-        var geometry = new THREE.BoxGeometry( 80, 80, 80 );
-        var material = new THREE.MeshStandardMaterial( {color: 0xf0fff0} );
+        var geometry = new THREE.BoxGeometry( 50, 50, 50 );
+        var material = new THREE.MeshStandardMaterial( {color: 0x0000ff} );
         var cube = new THREE.Mesh( geometry, material );
 
         return cube;
